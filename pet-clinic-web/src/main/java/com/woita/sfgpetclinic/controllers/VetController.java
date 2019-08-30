624669b492a77aa17d8249a1d2cb99bbabb61880
Rob McBryde <robertmcbryde83@gmail.com>
2019-08-30 15:25:08 +0100

    Addition of Vet Thymeleaf index page logic to list all vets. Closes #15

--- pet-clinic-web/src/main/java/com/woita/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/woita/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.woita.sfgpetclinic.controllers;
 
+import com.woita.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
