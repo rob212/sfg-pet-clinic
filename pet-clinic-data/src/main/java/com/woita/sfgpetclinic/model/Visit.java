43969f5c82b2b6f49454ade9fb73bb70ad2428c2
Rob McBryde <robertmcbryde83@gmail.com>
2019-09-06 17:03:04 +0100

    Added Pet to owner entity and created new Visit Entity. closes #26

---
+++ pet-clinic-data/src/main/java/com/woita/sfgpetclinic/model/Visit.java
@@ -0,0 +1,37 @@
+package com.woita.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author mcbrydr on 06/09/19
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
