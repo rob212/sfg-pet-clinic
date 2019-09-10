150a1f85fddb7b00c9d377d68bd569fdb5382365
Rob McBryde <robertmcbryde83@gmail.com>
2019-09-10 16:57:08 +0100

    Added SpecialityService and SpecialityMapService Impl to make use of base Abstract Crud methods. Closes #31

---
+++ pet-clinic-data/src/main/java/com/woita/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.woita.sfgpetclinic.services;
+
+import com.woita.sfgpetclinic.model.Speciality;
+
+/**
+ * @author mcbrydr on 10/09/19
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/woita/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,38 @@
+package com.woita.sfgpetclinic.services.map;
+
+import com.woita.sfgpetclinic.model.Speciality;
+import com.woita.sfgpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author mcbrydr on 10/09/19
+ */
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
