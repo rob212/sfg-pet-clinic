d75f91e222ddacfbd745901caaaadd76d199ab49
Rob McBryde <robertmcbryde83@gmail.com>
2019-09-16 17:09:28 +0100

    update visit with JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/com/woita/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/woita/sfgpetclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package com.woita.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * @author mcbrydr on 06/09/19
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
